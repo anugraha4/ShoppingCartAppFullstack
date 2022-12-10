ebcc8305dd8bedcc81cb209192c64c9eea657099
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-12-10 17:51:27 +0530

    api view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SearchComponent } from './search/search.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 const myRoute:Routes=[
  {
   path:"",
@@ -59,7 +60,8 @@ const myRoute:Routes=[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(myRoute),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
